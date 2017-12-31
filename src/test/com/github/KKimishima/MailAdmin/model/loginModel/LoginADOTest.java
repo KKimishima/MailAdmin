78694b86f4eeb3410309380b4cb1382a6e2412ee
君島克弥 <K.Kimishima123@gmail.com>
2017-12-31 17:34:47 +0900

    <変更>ログイン情報を保持できるように変更

--- src/test/com/github/KKimishima/MailAdmin/model/loginModel/LoginADOTest.java
+++ src/test/com/github/KKimishima/MailAdmin/model/loginModel/LoginADOTest.java
@@ -2,26 +2,38 @@ package com.github.KKimishima.MailAdmin.model.loginModel;
 
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 public class LoginADOTest {
-    /**
+
+  @Test
+  public void cleanLogin() throws Exception {
+    String userID = "0000";
+    String userPass = "PassAdmin";
+    LoginADO loginADO = new LoginADO(userID,userPass);
+    loginADO.loginCheck();
+    loginADO.cleanLogin();
+    assertThat(null,is(loginADO.getUserID()));
+  }
+
+
+  /**
    *
    * @throws Exception
    */
   @Test
   public void loginCheckTestユーザIDとユーザPassを与えて成功させる() throws Exception {
-    LoginADO loginADO = new LoginADO();
     String userID = "0000";
     String userPass = "PassAdmin";
-    assertEquals(true,loginADO.loginCheck(userID,userPass));
+    LoginADO loginADO = new LoginADO(userID,userPass);
+    assertEquals(true,loginADO.loginCheck());
   }
   @Test
   public void loginCheckTest失敗させる() throws Exception{
-    LoginADO loginADO = new LoginADO();
     String userID = "failID";
     String userPass = "failPass";
-    assertEquals(false,loginADO.loginCheck(userID,userPass));
+    LoginADO loginADO = new LoginADO(userID,userPass);
+    assertEquals(false,loginADO.loginCheck());
   }
-
 }
