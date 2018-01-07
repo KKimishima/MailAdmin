8faa3f2b7f5b9a1dee2e1e627e1e7699d3257f49
君島克弥 <K.Kimishima123@gmail.com>
2018-01-07 20:44:19 +0900

    <追加>成功メッセージ追加　refs #6 #17

--- src/main/java/com/github/KKimishima/MailAdmin/controller/Message.java
+++ src/main/java/com/github/KKimishima/MailAdmin/controller/Message.java
@@ -4,7 +4,9 @@ import javafx.scene.control.Alert;
 
 public class Message {
   private Alert alert;
+  private Alert sussed;
   public Message(){
+    this.sussed = new Alert(Alert.AlertType.INFORMATION);
     this.alert = new Alert(Alert.AlertType.ERROR);
   }
 
@@ -13,5 +15,12 @@ public class Message {
     alert.setHeaderText("エラーヘッダー");
     alert.setContentText("エラーコンテンツ");
     alert.show();
+  }
+
+  public void SussedMessga(){
+    sussed.setTitle("成功タイトル");
+    sussed.setHeaderText("成功ヘッダー");
+    sussed.setContentText("成功コンテンツ");
+    sussed.show();
   }
 }
