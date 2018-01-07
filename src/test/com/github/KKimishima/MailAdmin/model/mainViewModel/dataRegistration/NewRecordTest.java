745ef7345a1ffe315d1dffd8a7f0a2e07409fe6c
君島克弥 <K.Kimishima123@gmail.com>
2018-01-07 20:45:37 +0900

    <追加>新しくdbにレコード登録テスト追加(側だけ) refs #6 #17

---
+++ src/test/com/github/KKimishima/MailAdmin/model/mainViewModel/dataRegistration/NewRecordTest.java
@@ -0,0 +1,12 @@
+package com.github.KKimishima.MailAdmin.model.mainViewModel.dataRegistration;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class NewRecordTest {
+  @Test
+  public void DBRegister() throws Exception {
+  }
+
+}
