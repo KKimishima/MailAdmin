c39d07520b0b8153e80ea80f6eb51c25db983eb3
君島克弥 <K.Kimishima123@gmail.com>
2018-01-09 23:09:46 +0900

    <追加>主アドレスのみ新規登録機能実装 refs #7 #25

--- src/test/com/github/KKimishima/MailAdmin/model/mainViewModel/dataRegistration/NewRecordTest.java
+++ src/test/com/github/KKimishima/MailAdmin/model/mainViewModel/dataRegistration/NewRecordTest.java
@@ -1,12 +1,32 @@
 package com.github.KKimishima.MailAdmin.model.mainViewModel.dataRegistration;
 
+import com.github.KKimishima.MailAdmin.model.mainViewModel.viewInterFace.RegisterEnum;
+import com.github.KKimishima.MailAdmin.model.mainViewModel.viewInterFace.ViewStatus;
 import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class NewRecordTest {
+
   @Test
   public void DBRegister() throws Exception {
+    ViewStatus viewStatus = new ViewStatus();
+    viewStatus.setLoginUser("0000");
+    viewStatus.setName("javajva");
+    viewStatus.setAddress("javajvaj@javaasdgf.com");
+    viewStatus.setBikou("新規登録テスト");
+    viewStatus.setUserName("test");
+    viewStatus.setSyainID("1234");
+    viewStatus.setLocationST(1);
+    viewStatus.setPositionST(1);
+    viewStatus.setRegisterEnum(RegisterEnum.NEW);
+    viewStatus.setPrimaryAddressST(1);
+    viewStatus.setSecondaryAddressST(1);
+    viewStatus.setStatusRegisterST(1);
+
+    DBRegisterDAO dbRegisterDAO = new ChangeRecord(viewStatus);
+    assertThat(dbRegisterDAO.Register(),is(true));
   }
 
 }
